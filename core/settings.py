99a8ee5d84530df55c9e0f57575c4356d02403cb
rokeyaag <lutforitsolution@gmail.com>
2026-04-05 00:45:28 +0600

    add cors headers

--- core/settings.py
+++ core/settings.py
@@ -21,9 +21,11 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'django_filters',
     'store',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -86,3 +88,4 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 3,
 }
+CORS_ALLOW_ALL_ORIGINS = True
