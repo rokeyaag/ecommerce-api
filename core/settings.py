4d11286ff05cff6bb71c461bb2d791ca341a4179
rokeyaag <lutforitsolution@gmail.com>
2026-04-06 18:02:12 +0600

    increase page size

--- core/settings.py
+++ core/settings.py
@@ -86,6 +86,6 @@ REST_FRAMEWORK = {
         'rest_framework.filters.OrderingFilter',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 3,
+    'PAGE_SIZE': 100,
 }
 CORS_ALLOW_ALL_ORIGINS = True
